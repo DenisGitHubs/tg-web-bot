276072fded9d3f7d8259d53df351350d7760377c
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2024-02-11 17:54:56 +0700

    13

--- src/components/productList/productList.jsx
+++ src/components/productList/productList.jsx
@@ -1,85 +1,60 @@
-import React, { useState } from "react";
-import "./productList.css";
-import { ProductItem } from "../productItem/productItem";
-import { useTelegram } from "../../hooks/useTelegram";
-import './productList.css'
-const products = [
-    {
-        id: "1",
-        title: "Джинсы",
-        price: 5000,
-        description: "Синего цвета, прямые",
-    },
-    {
-        id: "2",
-        title: "Куртка",
-        price: 12000,
-        description: "Зеленого цвета, теплая",
-    },
-    {
-        id: "3",
-        title: "Джинсы 2",
-        price: 5000,
-        description: "Синего цвета, прямые",
-    },
-    {
-        id: "4",
-        title: "Куртка 8",
-        price: 122,
-        description: "Зеленого цвета, теплая",
-    },
-    {
-        id: "5",
-        title: "Джинсы 3",
-        price: 5000,
-        description: "Синего цвета, прямые",
-    },
-    {
-        id: "6",
-        title: "Куртка 7",
-        price: 600,
-        description: "Зеленого цвета, теплая",
-    },
-    {
-        id: "7",
-        title: "Джинсы 4",
-        price: 5500,
-        description: "Синего цвета, прямые",
-    },
-    {
-        id: "8",
-        title: "Куртка 5",
-        price: 12000,
-        description: "Зеленого цвета, теплая",
-    },
-];
+import React, { useCallback, useEffect, useState } from "react"
+import "./productList.css"
+import { ProductItem } from "../productItem/productItem"
+import { useTelegram } from "../../hooks/useTelegram"
+import "./productList.css"
+import { products } from "./productData"
 const getTotalPrice = (items) => {
     return items.reduce((acc, item) => {
-        return (acc += item.price);
-    }, 0);
-};
+        return (acc += item.price)
+    }, 0)
+}
+
 export const ProductList = () => {
-    const [addItems, setAddItems] = useState([]);
-    const { tg } = useTelegram();
-    const onAdd = (product) => {
-        const allreadyAdded = addItems.find((item) => item.id === product.id);
-        let newItems = [];
-        if (allreadyAdded) {
-            newItems = addItems.find((item) => item.id !== product.id);
-        } else {
-            newItems = [...addItems, product];
+    const [addItems, setAddItems] = useState([])
+    const { tg, queryId } = useTelegram()
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addItems,
+            totalPrice: getTotalPrice(addItems),
+            queryId,
         }
-        setAddItems(newItems);
+        fetch("http://localhost:8000", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+    }, [addItems])
+
+    useEffect(() => {
+        tg.onEvent("mainButtonClicked", onSendData)
+        return () => {
+            tg.offEvent("mainButtonClicked", onSendData)
+        }
+    }, [tg, onSendData])
+
+    const onAdd = (product) => {
+        const allreadyAdded = addItems.find((item) => item.id === product.id)
+        let newItems = []
+        if (allreadyAdded) {
+            newItems = addItems.find((item) => item.id !== product.id)
+        } else {
+            newItems = [...addItems, product]
+        }
+        setAddItems(newItems)
 
         if (newItems.length === 0) {
-            tg.MainButton.hide();
+            tg.MainButton.hide()
         } else {
-            tg.MainButton.show();
+            tg.MainButton.show()
             tg.MainButton.setParams({
                 text: `Купить ${getTotalPrice(newItems)}`,
-            });
+            })
         }
-    };
+    }
     return (
         <div>
             <div className="list">
@@ -92,5 +67,5 @@ export const ProductList = () => {
                 ))}
             </div>
         </div>
-    );
-};
+    )
+}
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,19 +1,20 @@
 const tg = window.Telegram.WebApp;
 export function useTelegram() {
-  const onClose = () => {
-    tg.close();
-  };
-  const onToggleButton = () => {
-    if (tg.MainButton.isVisible) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.show();
-    }
-  };
-  return {
-    onClose,
-    onToggleButton,
-    tg,
-    user: tg.initDataUnsafe?.user,
-  };
+    const onClose = () => {
+        tg.close();
+    };
+    const onToggleButton = () => {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    };
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id
+    };
 }
